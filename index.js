351434654fde7f5505cca546ec9d6057f84ca9c4
artiecannv <thefiftharthur@gmail.com>
2022-12-06 19:07:54 -0700

    added reminder for later

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 // TODO: Include packages needed for this application
-const generateMarkdown = require('./utils/generateMarkdown.js');
+const inquirer = require('inquirer');
+const util = require('./utils');
+const fs = require('fs');
 
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    'What is the Title of of the Project?',
+    'What is the Description of the Project',
+    'Table of Contents Needed?',
+    'Installation Needed?',
+    'Usage Needed?',
+    'License?',
+    'Contributors Needed?',
+    'Tests Needed?',
+    'Questions?'
+];
+
+inquirer
+    .createPromptModule([
+        {
+            type: 'input',
+            message: questions[0],
+            name: 'title',
+        }
+    ])
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -13,3 +34,4 @@ function init() {}
 
 // Function call to initialize app
 init();
+
